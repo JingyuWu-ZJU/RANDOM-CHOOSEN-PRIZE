652fe138609de1df3ff178d60431c7245f88f9bd
WuJingYu <32196157+WuJingYu-OUC@users.noreply.github.com>
2019-12-30 11:39:58 +0800

    Add files via upload
    随机抽奖的程序，VS2017，C#

---
+++ 胜楠摇奖程序/抽奖学程序/抽奖学程序/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace 抽奖学程序
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lab_NAME = new System.Windows.Forms.Label();
+            this.lab_prize = new System.Windows.Forms.Label();
+            this.btn_START = new System.Windows.Forms.Button();
+            this.btn_END = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lab_number = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lab_NAME
+            // 
+            this.lab_NAME.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lab_NAME.AutoSize = true;
+            this.lab_NAME.Font = new System.Drawing.Font("宋体", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lab_NAME.Location = new System.Drawing.Point(117, 59);
+            this.lab_NAME.Name = "lab_NAME";
+            this.lab_NAME.Size = new System.Drawing.Size(170, 70);
+            this.lab_NAME.TabIndex = 0;
+            this.lab_NAME.Text = "name";
+            this.lab_NAME.Click += new System.EventHandler(this.NAME_Click);
+            // 
+            // lab_prize
+            // 
+            this.lab_prize.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lab_prize.AutoSize = true;
+            this.lab_prize.Font = new System.Drawing.Font("宋体", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lab_prize.Location = new System.Drawing.Point(552, 64);
+            this.lab_prize.Name = "lab_prize";
+            this.lab_prize.Size = new System.Drawing.Size(205, 70);
+            this.lab_prize.TabIndex = 1;
+            this.lab_prize.Text = "prize";
+            // 
+            // btn_START
+            // 
+            this.btn_START.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_START.AutoSize = true;
+            this.btn_START.Font = new System.Drawing.Font("宋体", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_START.Location = new System.Drawing.Point(60, 286);
+            this.btn_START.Name = "btn_START";
+            this.btn_START.Size = new System.Drawing.Size(277, 134);
+            this.btn_START.TabIndex = 2;
+            this.btn_START.Text = "开始";
+            this.btn_START.UseVisualStyleBackColor = true;
+            this.btn_START.Click += new System.EventHandler(this.btn_START_Click);
+            // 
+            // btn_END
+            // 
+            this.btn_END.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_END.Font = new System.Drawing.Font("宋体", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_END.Location = new System.Drawing.Point(488, 286);
+            this.btn_END.Name = "btn_END";
+            this.btn_END.Size = new System.Drawing.Size(320, 134);
+            this.btn_END.TabIndex = 3;
+            this.btn_END.Text = "停止";
+            this.btn_END.UseVisualStyleBackColor = true;
+            this.btn_END.Click += new System.EventHandler(this.btn_END_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lab_number
+            // 
+            this.lab_number.AutoSize = true;
+            this.lab_number.Location = new System.Drawing.Point(422, 111);
+            this.lab_number.Name = "lab_number";
+            this.lab_number.Size = new System.Drawing.Size(0, 15);
+            this.lab_number.TabIndex = 4;
+            this.lab_number.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 528);
+            this.Controls.Add(this.lab_number);
+            this.Controls.Add(this.btn_END);
+            this.Controls.Add(this.btn_START);
+            this.Controls.Add(this.lab_prize);
+            this.Controls.Add(this.lab_NAME);
+            this.Name = "Form1";
+            this.Text = "马胜楠的摇奖小程序";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lab_NAME;
+        private System.Windows.Forms.Label lab_prize;
+        private System.Windows.Forms.Button btn_START;
+        private System.Windows.Forms.Button btn_END;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lab_number;
+    }
+}
+
---
+++ 胜楠摇奖程序/抽奖学程序/抽奖学程序/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace 抽奖学程序
+{
+    public partial class Form1 : Form
+    {
+        int i;
+        private Thread thread1;
+        delegate void set_Text(string s); //定义委托
+
+        set_Text Set_Text; //定义委托变更
+        int[] prize = { 1, 3, 5, 13, 4, 1, 2, 8, 45 };
+        string[] prize_name = { "一等奖", "二等奖", "三等奖", "合唱奖", "独唱奖", "独奏奖", "舞蹈奖", "游戏奖", "纪念奖" };
+        int[] count = new int[59];
+        string[] name = new string[59];
+        bool flag = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void NAME_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void set_lableText(string s)
+        {
+            lab_NAME.Text = s;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream("name.txt", FileMode.Open);
+            StreamReader read = new StreamReader(fs, Encoding.UTF8);
+            string strReadline;
+            int j = 0;
+            
+            while ((strReadline = read.ReadLine()) != null)
+            {
+                name[j] = strReadline;
+                j++;
+                
+            }
+            this.lab_NAME.Text = "获奖姓名";
+            this.lab_prize.Text = "奖项";
+            fs.Close();
+            read.Close();
+        }
+
+        private void btn_START_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = 10; //设置timer1的间隔时间    
+            timer1.Start(); //启动timer1     
+        }
+
+        private void btn_END_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            string temp_name;
+            int temp_prize;
+            temp_name = lab_NAME.Text;
+            temp_prize = Convert.ToInt32(lab_number.Text);
+            prize[temp_prize] = prize[temp_prize] - 1;
+            for (int j = 0; j < 59; j++)
+            {
+                if (name[j] == temp_name)
+                {
+                    name[j] = " ";
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random s1 = new Random();
+            Random s2 = new Random();
+            int ss1 = s1.Next(59);
+            int ss2 = s2.Next(9);
+            while (name[ss1] == " ")
+            {
+                ss1 = s1.Next(59);
+            }
+            this.lab_NAME.Text = name[ss1];
+            while (prize[ss2] == 0)
+            {
+                ss2 = s2.Next(9);
+            }
+            this.lab_number.Text = ss2.ToString();
+            this.lab_prize.Text = prize_name[ss2].ToString();
+        }
+    }
+}
